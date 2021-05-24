35626a240791ffea146351b545dd03288ff425f1
Samuel581 <00023020@uca.edu.sv>
2021-05-24 09:24:18 -0600

    Init

---
+++ Parcial2/UserControl1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Parcial2
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
